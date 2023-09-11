a8bd0a182b8ceac7d35c7a46a12859c067e23fac
Wisdom Ibu <wisdomibu@gmail.com>
2023-09-11 10:36:08 +0100

    Updated the time

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 const currentDay = document.getElementById("currentDayOfTheWeek");
 const Time = document.getElementById("currentTimeOfTheDay");
-//  ------ Days ----
-const dayOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
 
-const recentDate = new Date();
- const date = dayOfWeek[recentDate.getUTCDay()];
- currentDay.innerText = ` Today is ${date} `;
+function dateTime() {
+  const dayOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  
+  const recentDate = new Date();
+   const date = dayOfWeek[recentDate.getUTCDay()];
+   currentDay.innerText = ` Today is ${date} `;
+  
 
-// ------- Time ------
-const currentUTCTime = recentDate.getTime()
-Time.innerText = `Current UTC Time in Milliseconds: ${currentUTCTime}`;
+  const currentUTCTime = recentDate.getTime()
+  Time.innerText = `Current UTC Time in Milliseconds: ${currentUTCTime}`;
+}
+
+dateTime()
+
+setInterval(dateTime, 1000)
